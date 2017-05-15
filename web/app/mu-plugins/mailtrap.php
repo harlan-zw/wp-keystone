a0dca3623ef026190ed775c26315fe42d27c4c2d
Harlan Wilton <harlan.wilton@4mation.com.au>
2017-05-15 01:23:36 +0000

    mailtrap.php edited online with Bitbucket

--- web/app/mu-plugins/mailtrap.php
+++ web/app/mu-plugins/mailtrap.php
@@ -12,21 +12,19 @@ if (is_env_production()) {
     return;
 }
 
-
-$mailtrap_user = env('MAILTRAP_USER');
-$mailtrap_pass = env('MAILTRAP_USER');
-
-/**
- * Make sure that they've been defined
- */
-if(empty($mailtrap_user) || empty($mailtrap_pass)) {
-    return;
-}
-
 /**
  * Register our mailtrap override
  */
-add_action( 'phpmailer_init', function( PHPMailer $phpmailer ) use($mailtrap_user, $mailtrap_pass) {
+add_action( 'phpmailer_init', function( PHPMailer $phpmailer ) {
+
+    $mailtrap_user = env('MAILTRAP_USER');
+    $mailtrap_pass = env('MAILTRAP_PASS');
+    /**
+     * Make sure that they've been defined
+     */
+    if(empty($mailtrap_user) || empty($mailtrap_pass)) {
+        return;
+    }
 
     $phpmailer->Host = 'smtp.mailtrap.io';
     $phpmailer->Port = 2525; // could be different
@@ -34,6 +32,7 @@ add_action( 'phpmailer_init', function( PHPMailer $phpmailer ) use($mailtrap_use
     $phpmailer->Password = $mailtrap_pass; // if required
     $phpmailer->SMTPAuth = true; // if required
     $phpmailer->IsSMTP();
+    dd($mailtrap_user, $mailtrap_pass);
 
 }, PHP_INT_MAX);
 
