57a72d1e10ca2624e7df650f429d4c60efb9bcdb
Ruben Funai <ruben.funai@gmail.com>
2017-06-06 22:11:07 +1000

    Downgrad to bootstrap 3. Added script components.

---
+++ web/app/themes/sage/resources/assets/scripts/components/header.js
@@ -0,0 +1,5 @@
+export default {
+  ready: ($) => {
+      // Code to be run in browser
+  },
+};
--- web/app/themes/sage/resources/assets/scripts/main.js
+++ web/app/themes/sage/resources/assets/scripts/main.js
@@ -1,12 +1,24 @@
 /** import external dependencies */
 import 'jquery';
-import 'bootstrap';
+import 'bootstrap-sass';
 
 /** import local dependencies */
+import Components from './util/Components';
 import Router from './util/Router';
 import common from './routes/common';
 import home from './routes/home';
 import aboutUs from './routes/about';
+
+/** import components */
+// e.g. import header from  './components/header.js';
+
+/**
+ * Our extra components
+ * @type {Components}
+ */
+const components = new Components([
+    // e.g. header,
+]);
 
 /**
  * Populate Router instance with DOM routes
@@ -22,4 +34,9 @@ const routes = new Router({
 });
 
 /** Load Events */
-jQuery(document).ready(() => routes.loadEvents());
+jQuery(document).ready(($) => {
+    routes.loadEvents();
+    components.fire('ready', $);
+});
+
+jQuery(window).on('load', ($) => components.fire('loaded', $));
