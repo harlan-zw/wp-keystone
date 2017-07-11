e5da5ef019382edb8e11ce18d65f917da04512b9
harlan.wilton <harlan.wilton@4mation.com.au>
2017-07-11 12:15:03 +1000

    further updates

--- env/deploy/build-deployable-artifact.sh
+++ env/deploy/build-deployable-artifact.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-# Usage: build-deployable-artifact.sh "production" "boilerplate" "elasticbeanstalk-ap-southeast-2-xxxxxxxxxxxx"
+# Usage: build-deployable-artifact.sh "production"
 
 ENVIRONMENT=$1
-APPLICATION_NAME=$2
-S3_BUCKET=$3
+APPLICATION_NAME=$APPLICATION_NAME
+S3_BUCKET=$AWS_S3_BUCKET
 
+
+# Our artifact name is the last git revision
 GIT_HASH=$(git log --pretty=format:'%H' -n 1)
 FILENAME="$GIT_HASH.zip"
 MESSAGE=$(git log -1 --pretty=%B)
 
-zip builds/"$FILENAME" -r * .[^.]*
+zip "$FILENAME" -r * .[^.]*
 
-/root/.local/bin/aws s3 cp builds/"$FILENAME" s3://"$S3_BUCKET"/builds/"$FILENAME"
+/root/.local/bin/aws s3 cp "$FILENAME" s3://"$S3_BUCKET"/builds/"$FILENAME"
 
 /root/.local/bin/aws elasticbeanstalk create-application-version --application-name "$APPLICATION_NAME" --version-label "$GIT_HASH" --description "$MESSAGE" --source-bundle S3Bucket="$S3_BUCKET",S3Key="builds/$FILENAME"
 
