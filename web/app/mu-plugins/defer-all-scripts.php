88918b065017c83f2ec1f2f898fe5c7dee40bb57
Harlan <harlan103@hotmail.com>
2017-07-29 15:22:11 +1000

    deferring all script loading

---
+++ web/app/mu-plugins/defer-all-scripts.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Deferred Scripts
+Plugin URI: 4mation
+Description: All scripts loaded in are deferred until the DOM is rendered
+Version: 1.3
+Author: harlan
+*/
+
+add_filter('script_loader_tag', function($tag) {
+    // if they already have async or defer, then ignore
+    if (str_contains($tag,'async') || str_contains($tag,'defer')) {
+        return $tag;
+    }
+    return str_replace(' src', ' defer="defer" src', $tag);
+}, PHP_INT_MAX);
+
