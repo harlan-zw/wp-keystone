ac4a587cca3c0306c926157b559437d28c3a8f43
Harlan <harlan103@hotmail.com>
2018-08-30 23:01:38 +1000

    getting stylelint working and adding admin depedencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,32 +1,66 @@
-const mix = require('laravel-mix');
+/*
+ |--------------------------------------------------------------------------
+ | Laravel Mix
+ |--------------------------------------------------------------------------
+ |
+ | All assets are built using Laravel Mix. For full details on how to use it
+ | checkout the documentation:
+ |
+ | https://laravel.com/docs/5.6/mix
+ |
+ */
 
-mix
-    .setPublicPath('web')
-    .autoload({
-        jquery: ['$','jQuery', 'window.jQuery']
-    })
-.webpackConfig({
-    externals: {
-        "jquery": "jQuery"
-    }
-});
+const mix = require('laravel-mix');
+// custom packages
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 /*
  |--------------------------------------------------------------------------
- | Mix Asset Management
+ | Configure Mix
  |--------------------------------------------------------------------------
  |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application, as well as bundling up your JS files.
+ | Mix does about 80% of what we need it to do. Here we customise our paths
+ | and fill in the gaps for what mix isn't doing. Namely the jQuery dependency
+ | because it's loaded by WordPress and for simplicity we won't change this.
+ |
+ | We also setup style linting for our scss files.
  |
  */
-const distPath = 'web/dist';
-
 mix
-    .js('resources/assets/js/main.js', distPath)
-    .sass('resources/assets/scss/main.scss', distPath);
+// set web root
+    .setPublicPath('web')
+    // which files to pass to ProvidePlugin
+    .autoload({
+        jquery: ['$','jQuery', 'window.jQuery']
+    })
+    // extend webpack and put our own configuration
+    .webpackConfig({
+        // which js files are loaded outside of webpack
+        externals: {
+            // jQuery is loaded through WP
+            "jquery": "jQuery"
+        },
+        plugins: [
+            // make sure our scss is consistent
+            new StyleLintPlugin({
+                files: './resources/assets/scss/**/*.scss',
+            }),
+        ]
+    });
 
+/*
+ |--------------------------------------------------------------------------
+ | Mix configured based on environment
+ |--------------------------------------------------------------------------
+ |
+ | For our production environment we version our files so that we can implement
+ | cache bursting. We disable notifications as production servers will not have
+ | a gui.
+ |
+ | For our non-production environments we generate source maps so that we can
+ | debug our assets easier.
+ |
+ */
 if (mix.inProduction()) {
     mix.version();
     mix.disableNotifications();
@@ -34,40 +68,25 @@ if (mix.inProduction()) {
     mix.sourceMaps();
 }
 
-// Full API
-// mix.js(src, output);
-// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
-// mix.preact(src, output); <-- Identical to mix.js(), but registers Preact compilation.
-// mix.coffee(src, output); <-- Identical to mix.js(), but registers CoffeeScript compilation.
-// mix.ts(src, output); <-- TypeScript support. Requires tsconfig.json to exist in the same folder as webpack.mix.js
-// mix.extract(vendorLibs);
-// mix.sass(src, output);
-// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
-// mix.fastSass('src', output); <-- Alias for mix.standaloneSass().
-// mix.less(src, output);
-// mix.stylus(src, output);
-// mix.postCss(src, output, [require('postcss-some-plugin')()]);
-// mix.browserSync('my-site.test');
-// mix.combine(files, destination);
-// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
-// mix.copy(from, to);
-// mix.copyDirectory(fromDir, toDir);
-// mix.minify(file);
-// mix.sourceMaps(); // Enable sourcemaps
-// mix.version(); // Enable versioning.
-// mix.disableNotifications();
-// mix.setPublicPath('path/to/public');
-// mix.setResourceRoot('prefix/for/resource/locators');
-// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
-// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
-// mix.babelConfig({}); <-- Merge extra Babel configuration (plugins, etc.) with Mix's default.
-// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
-// mix.extend(name, handler) <-- Extend Mix's API with your own components.
-// mix.options({
-//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
-//   globalVueStyles: file, // Variables file to be imported in every component.
-//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
-//   purifyCss: false, // Remove unused CSS selectors.
-//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
-// });
+/*
+ |--------------------------------------------------------------------------
+ | Mix Build Assets
+ |--------------------------------------------------------------------------
+ |
+ | Build the assets now that we have configured mix. By default we have our
+ | main js/scss which is loaded by the frontend and the admin js/scss which
+ | is loaded when you are inside wp-admin.
+ |
+ */
+const distPath = 'web/dist';
+const assetPath = 'resources/assets';
+
+// frontend assets
+mix
+    .js(assetPath + '/js/main.js', distPath)
+    .sass(assetPath + '/scss/main.scss', distPath);
+
+// backend assets
+mix
+    .js(assetPath + '/js/admin.js', distPath)
+    .sass(assetPath + '/scss/admin.scss', distPath);
